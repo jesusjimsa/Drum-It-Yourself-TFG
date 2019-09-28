66409687fea35780787b439b4a820c216f89dffb
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-09-28 20:24:08 +0200

    Change from if-else to switch

--- src/main.c
+++ src/main.c
@@ -4,11 +4,6 @@
  *********************************************************************
 *********************************************************************/
 
-/*
-TODO:
-	PTP
-*/
-
 #include <ao/ao.h>
 #include <mpg123.h>
 #include <signal.h>
@@ -19,145 +14,108 @@ TODO:
 #define true (1 == 1)
 #define false (!true)
 
-/* enum instrument{
-	bass_drum,
-	closed_hi_hat,
-	crash_cymbal,
-	open_hi_hat,
-	ryde_cymbal,
-	snare_drum,
-	floor_tom,
-	mid_tom,
-	high_tom,
-	bass_and_closed_hh,
-	bass_and_open_hh,
-	closed_hh_and_snare,
-	crash_and_snare
-}; */
-
-void ChooseSound(char selected[]) {
-	if (selected[0] == '\n' && selected[1] == '\0') {
-		printf("You have to write one or more letter to make sounds\n");
-	}
-	else if (selected[0] == 'a' && selected[1] == '\n') {
-		play("sounds/bass_drum.mp3");
-	}
-	else if (selected[0] == 'b' && selected[1] == '\n') {
-		play("sounds/closed_hi_hat.mp3");
-	}
-	else if (selected[0] == 'c' && selected[1] == '\n') {
-		play("sounds/crash_cymbal.mp3");
-	}
-	else if (selected[0] == 'd' && selected[1] == '\n') {
-		play("sounds/floor_tom.mp3");
-	}
-	else if (selected[0] == 'e' && selected[1] == '\n') {
-		play("sounds/high_tom.mp3");
-	}
-	else if (selected[0] == 'f' && selected[1] == '\n') {
-		play("sounds/mid_tom.mp3");
-	}
-	else if (selected[0] == 'g' && selected[1] == '\n') {
-		play("sounds/open_hi_hat.mp3");
-	}
-	else if (selected[0] == 'h' && selected[1] == '\n') {
-		play("sounds/ryde_cymbal.mp3");
-	}
-	else if (selected[0] == 'i' && selected[1] == '\n') {
-		play("sounds/snare_drum.mp3");
-	}
-	else if ((selected[0] == 'a' && selected[1] == 'b' && selected[2] == '\n')
-			|| (selected[0] == 'b' && selected[1] == 'a' && selected[2] == '\n')) {
-		play("sounds/combined/bass_and_closed_hh.mp3");
-	}
-	else if ((selected[0] == 'a' && selected[1] == 'g' && selected[2] == '\n')
-			|| (selected[0] == 'g' && selected[1] == 'a' && selected[2] == '\n')) {
-		play("sounds/combined/bass_and_open_hh.mp3");
-	}
-	else if ((selected[0] == 'a' && selected[1] == 'i' && selected[2] == '\n')
-			|| (selected[0] == 'i' && selected[1] == 'a' && selected[2] == '\n')) {
-		play("sounds/combined/bass_and_snare.mp3");
-	}
-	else if ((selected[0] == 'b' && selected[1] == 'i' && selected[2] == '\n')
-			|| (selected[0] == 'i' && selected[1] == 'b' && selected[2] == '\n')) {
-		play("sounds/combined/closed_hh_and_snare.mp3");
-	}
-	else if ((selected[0] == 'c' && selected[1] == 'i' && selected[2] == '\n')
-			|| (selected[0] == 'i' && selected[1] == 'c' && selected[2] == '\n')) {
-		play("sounds/combined/crash_and_snare.mp3");
-	}
-	else if ((selected[0] == 'a' && selected[1] == 'd' && selected[2] == '\n')
-			|| (selected[0] == 'd' && selected[1] == 'a' && selected[2] == '\n')) {
-		play("sounds/combined/bass_and_floor.mp3");
-	}
-	else if ((selected[0] == 'b' && selected[1] == 'e' && selected[2] == '\n')
-			|| (selected[0] == 'e' && selected[1] == 'b' && selected[2] == '\n')) {
-		play("sounds/combined/bass_and_high.mp3");
-	}
-	else if ((selected[0] == 'c' && selected[1] == 'h' && selected[2] == '\n')
-			|| (selected[0] == 'h' && selected[1] == 'c' && selected[2] == '\n')) {
-		play("sounds/combined/crash_and_ryde.mp3");
-	}
-	else if ((selected[0] == 'e' && selected[1] == 'd' && selected[2] == '\n')
-			|| (selected[0] == 'd' && selected[1] == 'e' && selected[2] == '\n')) {
-		play("sounds/combined/high_and_floor_tom.mp3");
-	}
-	else if ((selected[0] == 'e' && selected[1] == 'f' && selected[2] == '\n')
-			|| (selected[0] == 'f' && selected[1] == 'e' && selected[2] == '\n')) {
-		play("sounds/combined/high_and_mid_tom.mp3");
-	}
-	else if ((selected[0] == 'f' && selected[1] == 'd' && selected[2] == '\n')
-			|| (selected[0] == 'd' && selected[1] == 'f' && selected[2] == '\n')) {
-		play("sounds/combined/mid_and_floor_tom.mp3");
-	}
-	else if ((selected[0] == 'g' && selected[1] == 'i' && selected[2] == '\n')
-			|| (selected[0] == 'i' && selected[1] == 'g' && selected[2] == '\n')) {
-		play("sounds/combined/open_hh_and_snare.mp3");
-	}
-	else if ((selected[0] == 'i' && selected[1] == 'd' && selected[2] == '\n')
-			|| (selected[0] == 'd' && selected[1] == 'i' && selected[2] == '\n')) {
-		play("sounds/combined/snare_and_floor_tom.mp3");
-	}
-	else if ((selected[0] == 'i' && selected[1] == 'e' && selected[2] == '\n')
-			|| (selected[0] == 'e' && selected[1] == 'i' && selected[2] == '\n')) {
-		play("sounds/combined/snare_and_high_tom.mp3");
-	}
-	else if ((selected[0] == 'i' && selected[1] == 'f' && selected[2] == '\n')
-			|| (selected[0] == 'f' && selected[1] == 'i' && selected[2] == '\n')) {
-		play("sounds/combined/snare_and_mid_tom.mp3");
-	}
-	else if ((selected[0] == 'i' && selected[1] == 'h' && selected[2] == '\n')
-			|| (selected[0] == 'h' && selected[1] == 'i' && selected[2] == '\n')) {
-		play("sounds/combined/snare_and_ryde.mp3");
-	}
-	else if (selected[0] == '*') {
-		return;
-	}
-	else{
-		printf("Not valid character!\n");
+void ChooseSound(int selected){
+	switch (selected){
+		case 1:
+			play("sounds/bass_drum.mp3");
+			break;
+		case 2:
+			play("sounds/closed_hi_hat.mp3");
+			break;
+		case 3:
+			play("sounds/crash_cymbal.mp3");
+			break;
+		case 4:
+			play("sounds/floor_tom.mp3");
+			break;
+		case 5:
+			play("sounds/high_tom.mp3");
+			break;
+		case 6:
+			play("sounds/mid_tom.mp3");
+			break;
+		case 7:
+			play("sounds/open_hi_hat.mp3");
+			break;
+		case 8:
+			play("sounds/ryde_cymbal.mp3");
+			break;
+		case 9:
+			play("sounds/snare_drum.mp3");
+			break;
+		case 10:
+			play("sounds/combined/bass_and_closed_hh.mp3");
+			break;
+		case 11:
+			play("sounds/combined/bass_and_open_hh.mp3");
+			break;
+		case 12:
+			play("sounds/combined/bass_and_snare.mp3");
+			break;
+		case 13:
+			play("sounds/combined/closed_hh_and_snare.mp3");
+			break;
+		case 14:
+			play("sounds/combined/crash_and_snare.mp3");
+			break;
+		case 15:
+			play("sounds/combined/bass_and_floor.mp3");
+			break;
+		case 16:
+			play("sounds/combined/bass_and_high.mp3");
+			break;
+		case 17:
+			play("sounds/combined/crash_and_ryde.mp3");
+			break;
+		case 18:
+			play("sounds/combined/high_and_floor_tom.mp3");
+			break;
+		case 19:
+			play("sounds/combined/high_and_mid_tom.mp3");
+			break;
+		case 20:
+			play("sounds/combined/mid_and_floor_tom.mp3");
+			break;
+		case 21:
+			play("sounds/combined/open_hh_and_snare.mp3");
+			break;
+		case 22:
+			play("sounds/combined/snare_and_floor_tom.mp3");
+			break;
+		case 23:
+			play("sounds/combined/snare_and_high_tom.mp3");
+			break;
+		case 24:
+			play("sounds/combined/snare_and_mid_tom.mp3");
+			break;
+		case 25:
+			play("sounds/combined/snare_and_ryde.mp3");
+			break;
+		case 0:
+			printf("Bye bye!\n");
+			break;	
+		default:
+			printf("You have to write a number between 0 and 25 (0 to exit)\n");
+			break;
 	}
 
 	kill(getpid(),SIGINT);
 }
 
 int main(int argc, char *argv[]) {
-	/*
-		The maximun is 5 (4 + '\0') because one person
-		can only play 4 instruments at the same time
-		when playing drums
-	*/
-	char selected[5];
+	int selected = 1;
 
 	/*
-		When reading a character from the keyboard, the
+		When reading a number from the keyboard, the
 		program will play a different drums sound.
 		It will change to drums hits in the final product.
 	 */
 	do{
-		fgets(selected, sizeof(selected), stdin);
+		scanf("%d", &selected);
 		
 		if (fork() == 0) {
 			ChooseSound(selected);
 		}
-	} while (selected[0] != '*');
+
+	} while (selected != 0);
 }
