d65ec58a45ebdd009b50590c46d7c88f2cc48f2e
jesusjimsa <jesusjimsa@icloud.com>
2019-12-01 18:54:56 +0100

    Read force in three intervals
    When it enters an interval, it will stop sending messages until force goes down.

--- force_sensor/force_sensor.ino
+++ force_sensor/force_sensor.ino
@@ -3,14 +3,41 @@
  *********************** Jesús Jiménez Sánchez ***********************
  *********************************************************************
 *********************************************************************/
-/* 
+/*
 	Arduino program to control the force sensor in  the drums
 */
 
-void setup() {
-	// put your setup code here, to run once:
+
+int fsrAnalogPin = 0;	// FSR is connected to analog 0
+int fsrReading;		// the analog reading from the FSR resistor divider
+
+int interval = false;		// [200, 400)
+
+void setup(void) {
+	Serial.begin(9600);	// We'll send debugging information via the Serial monitor
 }
 
-void loop() {
-	// put your main code here, to run repeatedly:
+void loop(void) {
+	fsrReading = analogRead(fsrAnalogPin);
+
+	if (!interval) {
+		if (fsrReading >= 200 && fsrReading < 400) {
+			interval = true;
+			Serial.println(fsrReading);
+		}
+		else if (fsrReading >= 400 && fsrReading < 800) {
+			interval = true;
+			Serial.println(fsrReading);
+		}
+		else if (fsrReading >= 800) {
+			interval = true;
+			Serial.println(fsrReading);
+		}
+	}
+
+	if (fsrReading < 100) {
+		interval = false;
+	}
+
+	delay(50);
 }
