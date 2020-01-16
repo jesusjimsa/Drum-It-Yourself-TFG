39987541a2859125db5e46e769b51ef0e471e846
jesusjimsa <jesusjimsa@icloud.com>
2020-01-16 21:40:06 +0100

    Simplify and prepare messages

--- force_sensor/force_sensor.ino
+++ force_sensor/force_sensor.ino
@@ -1,41 +1,37 @@
-/*********************************************************************
- *********************************************************************
- *********************** Jesús Jiménez Sánchez ***********************
- *********************************************************************
-*********************************************************************/
-/*
-	Arduino program to control the force sensor in  the drums
-*/
+/**
+ * Copyright 2020 Jesús Jiménez Sánchez
+ * 
+ * Arduino program to control the force sensor in  the drums
+ */
 
 
-int fsrAnalogPin = 0;	// FSR is connected to analog 0
+const int BUF_LEN = 10;
+
+const int fsrAnalogPin = 0;		// FSR is connected to analog 0
 int fsrReading;		// the analog reading from the FSR resistor divider
+int instrument = 1;
+int volume;
 
 int interval = false;		// [200, 400)
 
+char buf[BUF_LEN];
+int len = 0;
+
 void setup(void) {
-	Serial.begin(9600);	// We'll send debugging information via the Serial monitor
+	Serial.begin(9600);		// We'll send debugging information via the Serial monitor
 }
 
 void loop(void) {
 	fsrReading = analogRead(fsrAnalogPin);
-	
-	if (!interval) {
-		if (fsrReading >= 200) {
-			Serial.println("1:");
-		}
-		
-		if (fsrReading >= 200 && fsrReading < 400) {
-			interval = true;
-			Serial.println(fsrReading);
-		}
-		else if (fsrReading >= 400 && fsrReading < 800) {
-			interval = true;
-			Serial.println(fsrReading);
-		}
-		else if (fsrReading >= 800) {
-			interval = true;
-			Serial.println(fsrReading);
+	volume = 0;
+
+	if (!interval && fsrReading >= 200) {
+		interval = true;
+		// volume = fsrReading;
+		len = sprintf (buf, "%d:%d\n", instrument, fsrReading);
+
+		for(int i = 0; i <= len; i++) {
+			Serial.print(buf[i]);
 		}
 	}
 
